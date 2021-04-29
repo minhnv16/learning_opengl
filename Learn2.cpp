d2aed08caefdb07cfdfa64b2b6747c8ed0575ffd
Nguyen Van Minh <minh.nguyen@kohyoung.com>
2021-04-29 18:03:14 +0700

    uniform color setting

--- Learn2.cpp
+++ Learn2.cpp
@@ -9,17 +9,49 @@
 
 
 extern glm::mat4x4 matMVP2 = glm::mat4x4(1.0f);
+
+unsigned int aVecColorLocation;
+unsigned int aSelectedVecColorLocation;
+unsigned int aNormalVecColorLocation;
+
+glm::vec4 vecSelectedColor = glm::vec4(1.f, 0.f, 0.f, 1.f);
+glm::vec4 vecNormalColor = glm::vec4(0.f, 0.f, 1.f, 1.f);
+
 extern GLuint shaderProgram;
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 void key_callback_learn2(GLFWwindow* window, int key, int scancode, int action, int mode) {
 	if (action == GLFW_PRESS) {
-		unsigned int aMvpLocation = glGetUniformLocation(shaderProgram, "mvp");
-		//glm::mat4x4 matMVP = glm::mat4x4(1.0f);
-		matMVP2 = glm::translate(matMVP2, glm::vec3(0.1f, 0.1f, 0));
-		//matMVP2 = glm::scale(matMVP2, glm::vec3(1.1f, 1.1f, 0));
+		if (key == 32) {
+			glm::vec4 tmp = vecSelectedColor;
+			vecSelectedColor = vecNormalColor;
+			vecNormalColor = tmp;
+			glUniform4fv(aSelectedVecColorLocation, 1, glm::value_ptr(vecSelectedColor));
+			glUniform4fv(aNormalVecColorLocation, 1, glm::value_ptr(vecNormalColor));
 
-		glUniformMatrix4fv(aMvpLocation, 1, GL_FALSE, glm::value_ptr(matMVP2));
+		}
+
+		else if (key == 262) { //key right
+			unsigned int aMvpLocation = glGetUniformLocation(shaderProgram, "mvp");
+			//glm::mat4x4 matMVP = glm::mat4x4(1.0f);
+			matMVP2 = glm::translate(matMVP2, glm::vec3(0.1f, 0.1f, 0));
+			//matMVP2 = glm::scale(matMVP2, glm::vec3(1.1f, 1.1f, 0));
+
+			glUniformMatrix4fv(aMvpLocation, 1, GL_FALSE, glm::value_ptr(matMVP2));
+		}
+
+		else if (key == 263) {//key lef
+
+
+		}
+		else if (key == 264) {//key down
+
+
+		}
+		else if (key == 265) {//key up
+
+
+		}
 	}
 	else if (action == GLFW_RELEASE) {
 
@@ -57,21 +89,25 @@ int learn2() {
 	}
 
 	float vertices[] = {
-	0.f,0.f,0.f, 1.f,0.f,0.f,1.f,
+		//location				//selected					//selected
+		0.f,0.f,0.f,			0.f,
 
-	0.25f, 0.0f, 0.0f,  1.f,0.f,0.f,1.f,
-	0.25f, 0.25f, 0.0f, 0.f,1.f,0.f,1.f,
-	0.5f,  0.25f, 0.0f, 0.f,0.f,1.f,1.f,
-	0.5f,  0.0f, 0.0f,  1.f,0.f,1.f,1.f,
-	0.375f,  0.125f, 0.0f,  1.f,1.f,0.f,1.f,
+		0.25f, 0.0f, 0.0f,		1.f,
+		0.25f, 0.25f, 0.0f,		1.f,
+		0.5f,  0.25f, 0.0f,		1.f,
+		0.5f,  0.0f, 0.0f,		1.f,
+		0.375f,  0.125f, 0.0f,  1.f,
 
-	-0.25f, 0.0f, 0.0f,		1.f,0.f,1.f,1.f,
-	-0.25f, 0.25f, 0.0f,	0.f,1.f,0.f,1.f,
-	-0.5f,  0.25f, 0.0f,	0.f,0.f,1.f,1.f,
-	-0.5f,  0.0f, 0.0f,		1.f,1.f,0.f,1.f,
-	-0.375f,  0.125f, 0.0f,	0.f,1.f,1.f,1.f,
+		-0.25f, 0.0f, 0.0f,		0.f,
+		-0.25f, 0.25f, 0.0f,	0.f,
+		-0.5f,  0.25f, 0.0f,	0.f,
+		-0.5f,  0.0f, 0.0f,		0.f,
+		-0.375f,  0.125f, 0.0f,	0.f,
 
 	};
+
+
+
 
 	GLuint elements[] = {
 	1,2,3,4,5,
@@ -82,7 +118,7 @@ int learn2() {
 
 
 	//vertex buffer objects
-	unsigned int VBO;
+	GLuint VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
@@ -96,16 +132,26 @@ int learn2() {
 
 
 	char const* VertexShaderSource = R"GLSL(
-		#version 330
-		uniform mat4 mvp;
+		#version 130
+
 		in vec3 aPos;
-		in vec4 aColor;
+		uniform mat4 mvp;
+		in float  isSelected;
 		
 		out vec4 oColor; 
+		uniform vec4 uSelectedColor;
+		uniform vec4 uNormalColor;
+
 		void main()
 		{
+			if(isSelected==0.f){
+				oColor = uSelectedColor; // vec4(1.0f, 0.0f, 0.0f, 1.f);
+			}
+			else{
+				oColor = uNormalColor; // vec4(0.0f, 1.0f, 0.0f, 1.f);
+			}
 			gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-			oColor = aColor;
+
 		}
 		)GLSL";
 
@@ -124,9 +170,12 @@ int learn2() {
 
 
 	char const* FragmentShaderSource = R"GLSL(
-		#version 330
+		#version 130
+		uniform vec4 vecColor;
+
 
 		out vec4 FragColor;
+
 		in vec4 oColor; 
 
 		void main()
@@ -163,25 +212,37 @@ int learn2() {
 
 	//linking vertex attribute
 	unsigned int aPosLocation = glGetAttribLocation(shaderProgram, "aPos");
-	glVertexAttribPointer(aPosLocation, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), 0);
+	glVertexAttribPointer(aPosLocation, 3, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
 	glEnableVertexAttribArray(aPosLocation);
 
-	unsigned int aPosColor = glGetAttribLocation(shaderProgram, "aColor");
-	glVertexAttribPointer(aPosColor, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(aPosColor);
+
+	unsigned int aPosSelected = glGetAttribLocation(shaderProgram, "isSelected");
+	glVertexAttribPointer(aPosSelected, 1, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(aPosSelected);
 
 	unsigned int aMvpLocation = glGetUniformLocation(shaderProgram, "mvp");
 	glUniformMatrix4fv(aMvpLocation, 1, GL_FALSE, glm::value_ptr(matMVP2));
 
 
 
+	aVecColorLocation = glGetUniformLocation(shaderProgram, "vecColor");
+	aSelectedVecColorLocation = glGetUniformLocation(shaderProgram, "uSelectedColor");
+	aNormalVecColorLocation = glGetUniformLocation(shaderProgram, "uNormalColor");
 
 
 	glEnable(GL_PRIMITIVE_RESTART);
 	glPrimitiveRestartIndex(0);
 
+	int n = 0;
 	while (!glfwWindowShouldClose(window))
 	{
+
+		if (n < 2) {
+			glUniform4fv(aSelectedVecColorLocation, 1, glm::value_ptr(vecSelectedColor));
+			glUniform4fv(aNormalVecColorLocation, 1, glm::value_ptr(vecNormalColor));
+
+		}
+
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shaderProgram);
@@ -189,6 +250,15 @@ int learn2() {
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+
+		n++;
 	}
+
+
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteProgram(shaderProgram);
+	glfwTerminate();
+
 }
 
