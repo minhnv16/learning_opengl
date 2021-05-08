794f43d28fe87a49f7e27b197be6318e48e26973
minh nv <nguyenminhasp@gmail.com>
2021-05-08 09:32:39 +0700

    Fix error

--- Learn5.cpp
+++ Learn5.cpp
@@ -203,6 +203,8 @@ int main() {
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	glfwSetKeyCallback(window, key_callback_learn);
 
+	glewExperimental = GL_TRUE;
+
 	//init glew
 	GLenum err = glewInit();
 
